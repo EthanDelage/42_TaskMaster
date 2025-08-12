8d5f99424d8fc6950821f42f58af83d3cfd8cd3b
Ethan Delage <ethandelage04@gmail.com>
2025-08-12 23:55:02 +0200

    feat: implement basic Client class

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,8 @@
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+#include "client/Client.hpp"
+
+int main() {
+    Client client = Client("$> ");
+
+    client.loop();
     return 0;
 }
