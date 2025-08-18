00787c15a88ffde63245b0259e2abae2331b3624
Ethan Delage <ethandelage04@gmail.com>
2025-08-18 22:44:13 +0200

    refactor(Client): use an iterator to evaluate the command

--- include/client/Client.hpp
+++ include/client/Client.hpp
@@ -1,22 +1,38 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <functional>
 #include <string>
 #include <vector>
+
+typedef struct client_command_s    client_command_t;
+struct client_command_s {
+    std::string name;
+    std::string arg;
+    std::string doc;
+    std::function<void(const std::vector<std::string> &)> func;
+};
 
 class Client {
 public:
     explicit Client(std::string prompt_string);
     void loop();
-    void status();
+    void run_command(const std::vector<std::string> &args);
+    void status(const std::vector<std::string>&);
     void start(const std::vector<std::string>& args);
     void stop(const std::vector<std::string>& args);
     void restart(const std::vector<std::string>& args);
-    void reload();
+    void reload(const std::vector<std::string>&);
+    void quit(const std::vector<std::string>&);
+    void print_usage(const std::vector<std::string>&) const;
     static void print_header();
-    static void print_usage();
 private:
+    std::vector<client_command_t> _commands;
+    size_t _usage_max_len;
     std::string _prompt_string;
+    bool _is_running;
+
+    size_t get_usage_max_len() const;
 };
 
 #endif //CLIENT_HPP
--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,15 +1,29 @@
 #include "client/Client.hpp"
 
-#include <iostream>
 #include <common/utils.hpp>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 
-Client::Client(std::string prompt_string): _prompt_string(std::move(prompt_string)) {}
+Client::Client(std::string prompt_string): _prompt_string(std::move(prompt_string)), _is_running(true) {
+    _commands = {
+            {"status", "", "Show the status of all programs from the config file", [this](const std::vector<std::string>& args){ status(args); }},
+            {"start", "<name>", "Start the specified program", [this](const std::vector<std::string>& args){ start(args); }},
+            {"stop", "<name>", "Stop the specified program", [this](const std::vector<std::string>& args){ stop(args); }},
+            {"restart", "<name>", "Restart the specified program", [this](const std::vector<std::string>& args){ restart(args); }},
+            {"reload", "", "Reload the configuration file without stopping the shell", [this](const std::vector<std::string>& args){ reload(args); }},
+            {"quit", "", "Stop all programs and exit the shell", [this](const std::vector<std::string>& args){ quit(args); }},
+            {"exit", "", "Same as 'quit'", [this](const std::vector<std::string>& args){ quit(args); }},
+            {"help", "", "Show this help message", [this](const std::vector<std::string>& args){ print_usage(args); }},
+    };
+    _usage_max_len = get_usage_max_len();
+}
 
 void Client::loop() {
-    print_header();
-
     std::string input;
-    while (true) {
+
+    print_header();
+    while (_is_running) {
         std::cout << _prompt_string;
         if (!std::getline(std::cin, input)) {
             break; // EOF or error
@@ -19,61 +33,58 @@ void Client::loop() {
         if (args.empty()) {
             continue;
         }
-
-        const std::string& cmd = args[0];
-
-        if (cmd == "exit" || cmd == "quit") {
-            break;
-        }
-        if (cmd == "status") {
-            status();
-        } else if (cmd == "start") {
-            start(args);
-        } else if (cmd == "stop") {
-            stop(args);
-        } else if (cmd == "restart") {
-            restart(args);
-        } else if (cmd == "reload") {
-            reload();
-        } else if (cmd == "help") {
-            print_usage();
-        } else {
-            std::cout << "Unknown command: " << cmd << "\n";
-        }
+        run_command(args);
     }
 }
 
-void Client::status() {
+void Client::run_command(const std::vector<std::string> &args) {
+    const std::string& cmd = args[0];
+    for (auto & _command : _commands) {
+        if (_command.name == cmd) {
+            _command.func(args);
+            return;
+        }
+    }
+    std::cerr << "Unknown command: " << cmd << "\n";
+}
+
+
+void Client::status(const std::vector<std::string>&) {
     std::cout << "Status" << std::endl;
 }
 
 void Client::start(const std::vector<std::string>& args) {
     if (args.size() != 2) {
-        throw std::runtime_error("Invalid number of arguments\n"
-                                 "Usage: start <program_name>");
+        std::cerr << "Invalid number of arguments" << std::endl << "Usage: start <program_name>" << std::endl;
+        return;
     }
     std::cout << "Starting process: " << args[1] << std::endl;
 }
 
 void Client::stop(const std::vector<std::string>& args) {
     if (args.size() != 2) {
-        throw std::runtime_error("Invalid number of arguments\n"
-                                 "Usage: stop <program_name>");
+        std::cerr << "Invalid number of arguments" << std::endl << "Usage: stop <program_name>" << std::endl;
+        return;
     }
     std::cout << "Stopping process: " << args[1] << std::endl;
 }
 
 void Client::restart(const std::vector<std::string>& args) {
     if (args.size() != 2) {
-        throw std::runtime_error("Invalid number of arguments\n"
-                                 "Usage: restart <program_name>");
+        std::cerr << "Invalid number of arguments" << std::endl << "Usage: restart <program_name>" << std::endl;
+        return;
     }
     std::cout << "Restarting process: " << args[1] << std::endl;
 }
 
-void Client::reload() {
+void Client::reload(const std::vector<std::string>&) {
     std::cout << "Reloading config" << std::endl;
 }
+
+void Client::quit(const std::vector<std::string>&) {
+    _is_running = false;
+}
+
 
 void Client::print_header() {
     std::cout << "=====================================\n";
@@ -83,14 +94,29 @@ void Client::print_header() {
     std::cout << "Press Ctrl-D or Ctrl-C to quit the shell without stopping programs.\n\n";
 }
 
-void Client::print_usage() {
-    std::cout << "\nAvailable commands:\n"
-              << "  status                Show the status of all programs from the config file\n"
-              << "  start <name>          Start the specified program\n"
-              << "  stop <name>           Stop the specified program\n"
-              << "  restart <name>        Restart the specified program\n"
-              << "  reload                Reload the configuration file without stopping the shell\n"
-              << "  quit                  Stop all programs and exit the shell\n"
-              << "  exit                  Same as 'quit'\n"
-              << "  help                  Show this help message\n";
+void Client::print_usage(const std::vector<std::string>&) const {
+    std::cout << "Available commands:\n";
+    for (const auto& cmd : _commands) {
+        std::ostringstream left_part;
+        left_part << cmd.name;
+        if (!cmd.arg.empty()) {
+            left_part << " " << cmd.arg;
+        }
+
+        std::cout << "  "
+                  << std::left << std::setw(static_cast<int>(_usage_max_len) + 8)
+                  << left_part.str()
+                  << cmd.doc
+                  << std::endl;
+    }
 }
+
+size_t Client::get_usage_max_len() const {
+    size_t max_len = 0;
+    for (const auto& cmd : _commands) {
+        size_t len = cmd.name.size() + (cmd.arg.empty() ? 0 : 1 + cmd.arg.size());
+        max_len = std::max(max_len, len);
+    }
+    return max_len;
+}
+
