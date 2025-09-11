14eb55541a31164dc34d353cd80cd189dffe70d8
Ethan Delage <ethandelage04@gmail.com>
2025-09-11 13:47:30 +0200

    feat(server): implement basic UnixSocketServer class
    This class inherits from UnixSocket.
    The constructor configures the socket at the right address.
    This class contains two methods:
    * listen, which sets the numbers of connections pending and start listening for incoming connection
    * accept_client, which accepts the new connection and returns the new client fd

---
+++ src/server/UnixSocketServer.cpp
@@ -0,0 +1,41 @@
+#include "server/UnixSocketServer.hpp"
+
+#include <stdexcept>
+#include <sys/socket.h>
+#include <unistd.h>
+
+UnixSocketServer::UnixSocketServer(const std::string &path_name)
+    : UnixSocket(path_name) {
+  if (remove(path_name.c_str()) == -1 && errno != ENOENT) {
+    throw std::runtime_error(std::string("unix socket: ") + strerror(errno));
+  }
+  if (bind(_fd, reinterpret_cast<sockaddr *>(&_addr), sizeof(_addr)) == -1) {
+    throw std::runtime_error(std::string("bind: ") + strerror(errno));
+  }
+}
+
+UnixSocketServer::~UnixSocketServer() {
+  for (auto &fd : _listen_fds) {
+    if (close(fd) == -1) {
+      perror("close");
+    }
+  }
+}
+
+int UnixSocketServer::accept_client() {
+  int client_fd = accept(_fd, nullptr, nullptr);
+  if (client_fd == -1) {
+    perror("accept");
+    return -1;
+  }
+  _listen_fds.push_back(client_fd);
+  return client_fd;
+}
+
+int UnixSocketServer::listen(int backlog) const {
+  if (::listen(_fd, backlog) == -1) {
+    perror("listen");
+    return -1;
+  }
+  return 0;
+}
