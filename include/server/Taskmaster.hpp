5340a794cb6376943231052f9d0a7cdfc363bd96
Ethan Delage <ethandelage04@gmail.com>
2025-10-16 23:39:35 +0200

    feat(Taskmaster): add _client_sessions attribute

--- include/server/Taskmaster.hpp
+++ include/server/Taskmaster.hpp
@@ -2,6 +2,7 @@
 #define TASKMASTER_HPP
 
 #include "UnixSocketServer.hpp"
+#include "server/ClientSession.hpp"
 #include "server/Process.hpp"
 #include "server/config/Config.hpp"
 
@@ -31,15 +32,17 @@ private:
   std::unordered_map<std::string, std::vector<Process>> _processes_pool;
   std::vector<pollfd> _poll_fds;
   std::vector<poll_fd_metadata_t> _poll_fds_metadata;
+  std::vector<ClientSession> _client_sessions;
   UnixSocketServer _server_socket;
 
   void handle_poll_fds();
-  void handle_client_command();
+  void handle_client_command(const pollfd &poll_fd);
   void read_process_output(int fd);
   void handle_connection();
   void disconnect_client(int fd);
   void add_poll_fd(pollfd fd, poll_fd_metadata_t metadata);
   void remove_poll_fd(int fd);
+  void remove_client_session(int fd);
   static void set_sighup_handler();
 
   // Callback
@@ -52,6 +55,7 @@ private:
   void help(const std::vector<std::string> &args);
 
   // Getters
+  std::vector<ClientSession>::iterator get_client_session_from_fd(int fd);
   std::unordered_map<std::string, cmd_callback_t> get_commands_callback();
 };
 
