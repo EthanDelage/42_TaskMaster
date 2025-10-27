8e4fcfb4c1c3d840e8be1762864ce153c303c756
Ethan Delage <ethandelage04@gmail.com>
2025-10-28 00:25:29 +0100

    refactor(server): move _reload_requesting_clients to the ClientSession class
    Add a boolean to the ClientSession class to store whether the client has requested a reload

--- include/server/ClientSession.hpp
+++ include/server/ClientSession.hpp
@@ -13,8 +13,12 @@ public:
   std::string recv_command() const;
   void send_response(const std::string &response) const;
 
+  bool get_reload_request() const;
+  void set_reload_request(bool reload);
+
 private:
   static char _buffer[SOCKET_BUFFER_SIZE];
+  bool _reload_request;
 };
 
 #endif // CLIENTSESSION_HPP
