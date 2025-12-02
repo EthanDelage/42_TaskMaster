fd3c75c059790c77771af9a9d4f2ed2f44ca63e3
Ethan Delage <ethandelage04@gmail.com>
2025-12-03 00:26:16 +0100

    feat(Socket): add read/write variants with different parameters

--- src/common/socket/Socket.cpp
+++ src/common/socket/Socket.cpp
@@ -12,7 +12,23 @@ ssize_t Socket::read(char *buffer, size_t size) const {
 }
 
 ssize_t Socket::write(const std::string &buffer) const {
-  return ::write(_fd, buffer.c_str(), buffer.size());
+  return write(_fd, buffer);
+}
+
+ssize_t Socket::write(const char *buffer, size_t size) const {
+  return write(_fd, buffer, size);
+}
+
+ssize_t Socket::read(int fd, char *buffer, size_t size) {
+  return ::read(fd, buffer, size);
+}
+
+ssize_t Socket::write(int fd, const std::string &buffer) {
+  return write(fd, buffer.c_str(), buffer.size());
+}
+
+ssize_t Socket::write(int fd, const char *buffer, size_t size) {
+  return ::write(fd, buffer, size);
 }
 
 int Socket::get_fd() const { return _fd; }
